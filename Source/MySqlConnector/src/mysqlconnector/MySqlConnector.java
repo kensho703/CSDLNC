d3dc88060c09d6bc12613495627d7a24c91c8f55
kensho703 <36982844+kensho703@users.noreply.github.com>
2019-01-25 11:37:55 +0700

    Update MySqlConnector.java

--- Source/MySqlConnector/src/mysqlconnector/MySqlConnector.java
+++ Source/MySqlConnector/src/mysqlconnector/MySqlConnector.java
@@ -15,17 +15,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 
+/**
+ *
+ * @author kensho703
+ */
 public class MySqlConnector {
 
-//    private static String _dbHost = "10.240.194.44";
-//    private static String _dbPort = "33060";
-//    private static String _dbSid = "test";
-//    private static String _dbUsername = "root";
-//    private static String _dbPassword = "Viettel@123";
     private static String _dbHost = null;
     private static String _dbPort = null;
     private static String _dbSid = null;
@@ -52,24 +52,105 @@ public class MySqlConnector {
             _dbUsername = scanner.nextLine();
             System.out.println("enter Db_Pass:");
             _dbPassword = scanner.nextLine();
-             System.out.println("enter SQL query:");
-             String query = scanner.nextLine();
 
             conn = getMySqlConnection(_dbHost, _dbPort, _dbSid, _dbUsername, _dbPassword);
             st = conn.createStatement();
-//            String query = "select * from tbl_test where 1 = 1 limit 1000";
-            pstmt = conn.prepareStatement(query);
-            ResultSet rs = pstmt.executeQuery();
-            ResultSetMetaData metaData = rs.getMetaData();
-            int count = metaData.getColumnCount(); // get column count
-            rs.next();
-            while (rs.next()) {
-                for (int i = 1; i <= count; i++) {
-                    System.out.print(metaData.getColumnLabel(i) + " = " + rs.getString(i) + " | ");
+
+            scanner = new Scanner(System.in);
+            System.out.println("Enter number to choose option: ");
+            System.out.println("0. Exit ");
+            System.out.println("1. Select");
+            System.out.println("2. Insert");
+            System.out.println("3. Delete");
+            int option = scanner.nextInt();
+
+            while (option != 0) {
+                switch (option) {
+                    case 1: {
+                        scanner = new Scanner(System.in);
+                        System.out.println("enter SQL query:");
+                        String query = scanner.nextLine();
+                        pstmt = conn.prepareStatement(query);
+                        long startTime = System.currentTimeMillis();
+                        ResultSet rs = pstmt.executeQuery();
+                        ResultSetMetaData metaData = rs.getMetaData();
+                        rs.next();
+                        while (rs.next()) {
+                        }
+                        long endTime = System.currentTimeMillis();
+                        long duration = endTime - startTime;
+                        System.out.println("Executed time of query : " + duration + " ms");
+
+                        System.out.println("Enter number to choose option: ");
+                        System.out.println("0. Exit ");
+                        System.out.println("1. Select");
+                        System.out.println("2. Insert");
+                        System.out.println("3. Delete");
+                        option = scanner.nextInt();
+                    }
+
+                    break;
+                    case 2: {
+                        scanner = new Scanner(System.in);
+                        System.out.println("enter SQL query:");
+                        String query = scanner.nextLine();
+                        long startTime = System.currentTimeMillis();
+                        pstmt = conn.prepareStatement(query);
+                        pstmt.executeUpdate();
+                        long endTime = System.currentTimeMillis();
+                        long duration = endTime - startTime;
+                        System.out.println("Executed time of query : " + duration + " ms");
+
+                        System.out.println("Enter number to choose option: ");
+                        System.out.println("0. Exit ");
+                        System.out.println("1. Select");
+                        System.out.println("2. Insert");
+                        System.out.println("3. Delete");
+                        option = scanner.nextInt();
+                    }
+                    break;
+                    case 3: {
+                        scanner = new Scanner(System.in);
+                        System.out.println("enter SQL query:");
+                        String query = scanner.nextLine();
+                        long startTime = System.currentTimeMillis();
+                        pstmt = conn.prepareStatement(query);
+                        pstmt.executeUpdate();
+                        long endTime = System.currentTimeMillis();
+                        long duration = endTime - startTime;
+                        System.out.println("Executed time of query : " + duration + " ms");
+
+                        System.out.println("Enter number to choose option: ");
+                        System.out.println("0. Exit ");
+                        System.out.println("1. Select");
+                        System.out.println("2. Insert");
+                        System.out.println("3. Delete");
+                        option = scanner.nextInt();
+                    }
+                    break;
+                    default:
+                        throw new AssertionError();
                 }
-                System.out.print("\n");
             }
 
+////            String query = "select * from tbl_test where 1 = 1 limit 1000";
+//            st = conn.createStatement();
+//            pstmt = conn.prepareStatement(query);
+//            long startTime = System.currentTimeMillis();
+//            ResultSet rs = pstmt.executeQuery();
+//            ResultSetMetaData metaData = rs.getMetaData();
+////            int count = metaData.getColumnCount(); // get column count
+//            rs.next();
+//            while (rs.next()) {
+////                for (int i = 1; i <= count; i++) {
+////                    System.out.print(metaData.getColumnLabel(i) + " = " + rs.getString(i) + " | ");
+////                }
+////                rs.getString(i);
+////                System.out.print("\n");
+//            }
+//            long endTime = System.currentTimeMillis();
+//            long duration = endTime - startTime;
+//            System.out.println("Executed time of query : " + duration + " ms");
         } catch (Exception ex) {
             Logger.getLogger(MySqlConnector.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
@@ -94,7 +175,7 @@ public class MySqlConnector {
 
     public static Connection getMySqlConnection(String host, String port, String sid, String username, String password) throws Exception {
         String driver = "com.mysql.jdbc.Driver";
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + sid + "?autoReconnect=true&useSSL=false";
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + sid + "?autoReconnect=true&allowPublicKeyRetrieval=true&useSSL=false";
         Class.forName(driver);
         Connection conn = DriverManager.getConnection(url, username, password);
         return conn;
